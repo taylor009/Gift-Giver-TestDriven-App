5d3e041212c96096d2501be0327e047c25aa8a18
Taylor Gagne <taylor@baremetalcloud.com>
2018-05-05 16:19:01 -0400

    Added styling to project to make it complete

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -9,7 +9,7 @@ class Gift extends React.Component{
   }
   render(){
     return(
-      <div>
+      <div className='gift'>
         <Form>
           <FormGroup>
             <ControlLabel>Person</ControlLabel>
