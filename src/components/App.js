c41e94f28be766ae119ee874b5caedcf64fb93a7
Taylor Gagne <taylor@baremetalcloud.com>
2018-05-05 02:24:14 -0400

    Created an add gift button along with test case for the button.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
 
 class App extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {gifts: []};
+  }
+
+  addGift = () => {
+    const {gifts} = this.state;
+
+    const ids = this.state.gifts.map(gift => gift.id);
+
+    const max_id = ids.length > 0 ? Math.max(...ids) : 0;
+
+    gifts.push({id: max_id+1});
+
+    this.setState({gifts});
+  }
+
   render(){
     return(
       <div>
         <h2>Gift Giver</h2>
+        <Button onClick={this.addGift} className='btn-add'>Add Gift</Button>
       </div>
     )
   }
